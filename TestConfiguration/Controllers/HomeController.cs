c148f9ca09186bfd8c4df196bbd99157d4935858
yowko <yowko0729@gmail.com>
2017-02-08 17:09:45 +0800

    初步完成第一版

---
+++ TestConfiguration/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestConfiguration.Handler;
+
+namespace TestConfiguration.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            YowkoConfigSectionHandler clientConfiguration = ConfigurationManager.GetSection("yowkoDataGroup/yowkoData") as YowkoConfigSectionHandler;
+
+            return View(clientConfiguration);
+        }
+    }
+}
---
+++ TestConfiguration/Handler/YowkoConfigSectionHandler.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace TestConfiguration.Handler
+{
+    public class YowkoConfigSectionHandler : ConfigurationSection
+    {
+        [ConfigurationProperty("channel", DefaultValue = "", IsRequired = true)]
+        public string Channel
+        {
+            get
+            {
+                return this["channel"] as string;
+            }
+            set
+            {
+                this["channel"] = value;
+            }
+        }
+
+        [ConfigurationProperty("userName", IsRequired = true)]
+        public string UserName
+        {
+            get
+            {
+                return this["userName"] as string;
+            }
+            set
+            {
+                this["userName"] = value;
+            }
+
+        }
+
+        [ConfigurationProperty("userSecret", IsRequired = true)]
+        public string UserSecret
+        {
+            get
+            {
+                return this["userSecret"] as string;
+            }
+            set
+            {
+                this["userSecret"] = value;
+            }
+        }
+    }
+
+    public class YowkoDataElement : ConfigurationElement
+    {
+        [ConfigurationProperty("channel", DefaultValue = "", IsRequired = true)]
+        public string Channel
+        {
+            get
+            {
+                return this["channel"] as string;
+            }
+            set
+            {
+                this["channel"] = value;
+            }
+        }
+
+        [ConfigurationProperty("userName", IsRequired = true)]
+        public string UserName
+        {
+            get
+            {
+                return this["userName"] as string;
+            }
+            set
+            {
+                this["userName"] = value;
+            }
+
+        }
+
+        [ConfigurationProperty("userSecret", IsRequired = true)]
+        public string UserSecret
+        {
+            get
+            {
+                return this["userSecret"] as string;
+            }
+            set
+            {
+                this["userSecret"] = value;
+            }
+        }
+    }
+
+}
